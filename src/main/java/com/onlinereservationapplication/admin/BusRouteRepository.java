34e60bcb77540a7ae46dec87ebc9194e9ac132ee
Manikanta Majeti <mmk4mmk.mrani@gmail.com>
2024-05-06 10:59:04 +0100

    "Remove unused import statements "

--- src/main/java/com/onlinereservationapplication/admin/BusRouteRepository.java
+++ src/main/java/com/onlinereservationapplication/admin/BusRouteRepository.java
@@ -1,7 +1,5 @@
 package com.onlinereservationapplication.admin;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BusRouteRepository extends JpaRepository<Busroute, String>
